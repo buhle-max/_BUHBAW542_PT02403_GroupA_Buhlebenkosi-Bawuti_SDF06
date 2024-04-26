8697c2596bb7dffd8019bd8dc78591086a9ce0a5
Buhle <buhlebawuti44@gmail.com>
2024-04-26 16:34:07 +0200

    modifies HTML and CSS and JavaScript.

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from  "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from  "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+const appSettings = {
+    databaseURL: "https://realtime-database-1365b-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListDB = ref(database, "shoppingList")
+
+const inputFildEl = document.getElementById("input-field")
+const addingButtonEl = document.getElementById("add-button")
+const shoppListEl = document.getElementById("shopping-list")
+
+
+addingButtonEl.addEventListener("click", function() {
+    let inputValue = inputFildEl.value
     
-    console.log(inputValue)
+    push(shoppingListDB, inputValue)
+    
+    shoppListEl.innerHTML += `<li>${inputValue}</li>`
+    clearinputFildEl()
 })
+
+onValue(shoppingListDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearShoppListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToShoppingListEl(currentItem)
+        }    
+    } else {
+        shoppListEl.innerHTML = "No items here... yet"
+    }
+})
+
+function clearShoppListEl() {
+    shoppListEl.innerHTML = ""
+}
+
+function clearInputFildEl() {
+    inputFildEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    shoppListEl.append(newEl) }
